280e214d357732368ce1d751ef018b083e6dd0a9
biddal <biddal_2@hotmail.com>
2017-07-27 07:59:04 +0200

    site ok with bug js for the list

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -43,9 +43,31 @@ class IndexController extends Controller
         return $this->render('resultat.html.twig',
                 array('events'=> $events, 
                       'validate' => $events0
-                ));
-
+                ));       
+    }
+    /**
+     * @Route("/classement", name="classement")
+     */
+    
+    public function classementAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        // Native query for general classment
+        $sql = "SELECT SUM(result.point) as total, SUM(result.times) as ttime, user.lastname, user.firstname "
+                . "FROM result "
+                . "inner join user on result.id_user = user.id "
+                . "inner join run on result.id_run = run.id "
+                . "inner join type on user.typ_id = type.id "
+                . "WHERE YEAR(CURRENT_DATE()) = 2017 "
+                . "GROUP BY user.id "
+                . "ORDER BY total DESC ";
+        $nativeQuery = $em->getConnection()->prepare($sql);
+        $nativeQuery->execute();
+        $general_classment = $nativeQuery->fetchAll();
         
         
+        return $this->render('classement.html.twig',
+                array('generals'          => $general_classment  
+                ));       
     }
 }
--- web/js/ccca1cb.js
+++ web/js/ccca1cb.js
@@ -35,3 +35,262 @@ $(document).ready(function( ) {
         });
     });
 });
+$(document).ready(function( ) {
+    
+    $( ".js-datepicker" ).datepicker({
+        dateFormat: "yy/mm/dd"
+    });
+    
+    $(document).on("change", "#eventid" ,function() {
+        $selected = "#" + $(this).val();
+        $(".formTime").not($selected).each(function() {
+            $(".active").slideUp("1500");
+            $(".active").removeClass("active");
+        })
+            
+        $($selected).toggleClass("active");
+        $($selected).slideToggle("1500");
+        //console.log($selected);
+    });
+    
+    $(document).on("change", ".time" ,function() {
+        var id = $(this).attr('id');        
+        var idcoef = $('#coef'+id).html();
+        var run = $(this).parent().parent().parent().attr('id');
+        console.log(run);
+        var calcul = Math.floor((1000/$(this).val())*idcoef);
+        $("#point"+id).html(calcul);
+        $.ajax({
+            type: "POST",
+            url: "",
+            data: {
+                iduser: id,
+                time: $(this).val(),
+                point: calcul,
+                idrun: run
+            }
+        });
+    });
+});
+$(document).ready(function( ) {
+    
+    $( ".js-datepicker" ).datepicker({
+        dateFormat: "yy/mm/dd"
+    });
+    
+    $(document).on("change", "#eventid" ,function() {
+        $selected = "#" + $(this).val();
+        $(".formTime").not($selected).each(function() {
+            $(".active").slideUp("1500");
+            $(".active").removeClass("active");
+        })
+            
+        $($selected).toggleClass("active");
+        $($selected).slideToggle("1500");
+        //console.log($selected);
+    });
+    
+    $(document).on("change", ".time" ,function() {
+        var id = $(this).attr('id');        
+        var idcoef = $('#coef'+id).html();
+        var run = $(this).parent().parent().parent().attr('id');
+        console.log(run);
+        var calcul = Math.floor((1000/$(this).val())*idcoef);
+        $("#point"+id).html(calcul);
+        $.ajax({
+            type: "POST",
+            url: "",
+            data: {
+                iduser: id,
+                time: $(this).val(),
+                point: calcul,
+                idrun: run
+            }
+        });
+    });
+});
+$(document).ready(function( ) {
+    
+    $( ".js-datepicker" ).datepicker({
+        dateFormat: "yy/mm/dd"
+    });
+    
+    $(document).on("change", "#eventid" ,function() {
+        $selected = "#" + $(this).val();
+        $(".formTime").not($selected).each(function() {
+            $(".active").slideUp("1500");
+            $(".active").removeClass("active");
+        })
+            
+        $($selected).toggleClass("active");
+        $($selected).slideToggle("1500");
+        //console.log($selected);
+    });
+    
+    $(document).on("change", ".time" ,function() {
+        var id = $(this).attr('id');        
+        var idcoef = $('#coef'+id).html();
+        var run = $(this).parent().parent().parent().attr('id');
+        console.log(run);
+        var calcul = Math.floor((1000/$(this).val())*idcoef);
+        $("#point"+id).html(calcul);
+        $.ajax({
+            type: "POST",
+            url: "",
+            data: {
+                iduser: id,
+                time: $(this).val(),
+                point: calcul,
+                idrun: run
+            }
+        });
+    });
+});
+$(document).ready(function( ) {
+    
+    $( ".js-datepicker" ).datepicker({
+        dateFormat: "yy/mm/dd"
+    });
+    
+    $(document).on("change", "#eventid" ,function() {
+        $selected = "#" + $(this).val();
+        $(".formTime").not($selected).each(function() {
+            $(".active").slideUp("1500");
+            $(".active").removeClass("active");
+        })
+            
+        $($selected).toggleClass("active");
+        $($selected).slideToggle("1500");
+        //console.log($selected);
+    });
+    
+    $(document).on("change", ".time" ,function() {
+        var id = $(this).attr('id');        
+        var idcoef = $('#coef'+id).html();
+        var run = $(this).parent().parent().parent().attr('id');
+        console.log(run);
+        var calcul = Math.floor((1000/$(this).val())*idcoef);
+        $("#point"+id).html(calcul);
+        $.ajax({
+            type: "POST",
+            url: "",
+            data: {
+                iduser: id,
+                time: $(this).val(),
+                point: calcul,
+                idrun: run
+            }
+        });
+    });
+});
+$(document).ready(function( ) {
+    
+    $( ".js-datepicker" ).datepicker({
+        dateFormat: "yy/mm/dd"
+    });
+    
+    $(document).on("change", "#eventid" ,function() {
+        $selected = "#" + $(this).val();
+        $(".formTime").not($selected).each(function() {
+            $(".active").slideUp("1500");
+            $(".active").removeClass("active");
+        })
+            
+        $($selected).toggleClass("active");
+        $($selected).slideToggle("1500");
+        //console.log($selected);
+    });
+    
+    $(document).on("change", ".time" ,function() {
+        var id = $(this).attr('id');        
+        var idcoef = $('#coef'+id).html();
+        var run = $(this).parent().parent().parent().attr('id');
+        console.log(run);
+        var calcul = Math.floor((1000/$(this).val())*idcoef);
+        $("#point"+id).html(calcul);
+        $.ajax({
+            type: "POST",
+            url: "",
+            data: {
+                iduser: id,
+                time: $(this).val(),
+                point: calcul,
+                idrun: run
+            }
+        });
+    });
+});
+$(document).ready(function( ) {
+    
+    $( ".js-datepicker" ).datepicker({
+        dateFormat: "yy/mm/dd"
+    });
+    
+    $(document).on("change", "#eventid" ,function() {
+        $selected = "#" + $(this).val();
+        $(".formTime").not($selected).each(function() {
+            $(".active").slideUp("1500");
+            $(".active").removeClass("active");
+        })
+            
+        $($selected).toggleClass("active");
+        $($selected).slideToggle("1500");
+        //console.log($selected);
+    });
+    
+    $(document).on("change", ".time" ,function() {
+        var id = $(this).attr('id');        
+        var idcoef = $('#coef'+id).html();
+        var run = $(this).parent().parent().parent().attr('id');
+        console.log(run);
+        var calcul = Math.floor((1000/$(this).val())*idcoef);
+        $("#point"+id).html(calcul);
+        $.ajax({
+            type: "POST",
+            url: "",
+            data: {
+                iduser: id,
+                time: $(this).val(),
+                point: calcul,
+                idrun: run
+            }
+        });
+    });
+});
+$(document).ready(function( ) {
+    
+    $( ".js-datepicker" ).datepicker({
+        dateFormat: "yy/mm/dd"
+    });
+    
+    $(document).on("change", "#eventid" ,function() {
+        $selected = "#" + $(this).val();
+        $(".formTime").not($selected).each(function() {
+            $(".active").slideUp("1500");
+            $(".active").removeClass("active");
+        })
+            
+        $($selected).toggleClass("active");
+        $($selected).slideToggle("1500");
+        //console.log($selected);
+    });
+    
+    $(document).on("change", ".time" ,function() {
+        var id = $(this).attr('id');        
+        var idcoef = $('#coef'+id).html();
+        var run = $(this).parent().parent().parent().attr('id');
+        console.log(run);
+        var calcul = Math.floor((1000/$(this).val())*idcoef);
+        $("#point"+id).html(calcul);
+        $.ajax({
+            type: "POST",
+            url: "",
+            data: {
+                iduser: id,
+                time: $(this).val(),
+                point: calcul,
+                idrun: run
+            }
+        });
+    });
+});
